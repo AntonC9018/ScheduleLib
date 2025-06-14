3d9865eee57b2e95d23214020787d00c5e54d5ac
AntonC9018 <anton.curm@gmail.com>
2025-06-14 16:45:52 +0300

    Don't use absolute paths

--- src/Comisia/Program.cs
+++ src/Comisia/Program.cs
@@ -1,8 +1,8 @@
 ﻿
 using Comisia;
 
-var commissions = CommissionParser.ParseCommissions(@"C:\Users\Anton\Downloads\commissions.xlsx");
-var theses = ThesisListParser.Parse(@"C:\Users\Anton\Downloads\thesis_distribution.xlsx", ThesisType.Licenta);
+var commissions = CommissionParser.ParseCommissions("data/commissions.xlsx");
+var theses = ThesisListParser.Parse("data/thesis_distribution.xlsx", ThesisType.Licenta);
 var matchedData = DataMatcher.Match(commissions, theses);
 ReportGenerator.GenerateReport(new()
 {
