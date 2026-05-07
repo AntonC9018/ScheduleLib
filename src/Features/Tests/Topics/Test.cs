330f3f9677a997de3d640933ee77c4203370338c
AntonC9018 <anton.curm@gmail.com>
2026-05-08 01:28:08 +0300

    Debug

---
+++ src/Features/Tests/Topics/Test.cs
@@ -0,0 +1,71 @@
+using Microsoft.Extensions.DependencyInjection;
+using ScheduleFromDoc.Tests;
+using ScheduleLib.Application.Config;
+using ScheduleLib.Application.Core.Topics;
+using ScheduleLib.Builders;
+using ScheduleLib.OnlineRegistry;
+using ScheduleLib.Parsing;
+
+namespace ScheduleLib.Topics.Tests;
+
+public sealed class Test
+{
+    [Fact]
+    public async Task TestStuff()
+    {
+        using var helper = await IntegrationTestHelper.CreateNew();
+        var schedule = helper.GetScheduleFromSourceOfTruth();
+        var lookup = helper.ServiceProvider.GetRequiredService<LookupFacade>();
+        var courseId = lookup.Course("Programare în C++".AsMemory())!.Value;
+
+        var teachers = schedule.EnumerateWeeklyLessons()
+            .Where(x => x.Lesson.Course == courseId)
+            .Select(x => x.Lesson.Teachers)
+            .SelectMany(x => x)
+            .Distinct();
+
+        var teacherId = teachers.First();
+        var teacherName = schedule.Get(teacherId).PersonName.AsNameFields();
+        var manifestFileSource = ActivatorUtilities.CreateInstance<ManifestFileSource>(
+            helper.ServiceProvider,
+            "data/manifest.json",
+            new Name(teacherName));
+
+        var manifest = await manifestFileSource.Read(helper.CancellationToken);
+        var filteredSchedule = schedule.Filter(
+            new ScheduleFilter()
+                .WithCourse(courseId)
+                .WithTeacher(teacherId));
+
+        var builder = ActivatorUtilities.CreateInstance<AllLessonTopicsDatabaseBuilder>(helper.ServiceProvider, filteredSchedule);
+        await builder.AddFromManifest(manifest, lookup, helper.CancellationToken);
+        var result = builder.Build();
+
+        var list = new List<string>();
+        int dayIndex = 0;
+        var groups = new FoundGroups
+        {
+            Value = new(),
+            IsWildcard = false,
+        };
+        while (true)
+        {
+            var topic = result.Get(new(
+                courseId: courseId,
+                groups: groups,
+                subGroup: SubGroup.All,
+                lessonType: LessonType.Lab,
+                dayIndex: dayIndex,
+                dateTime: default));
+            if (topic == null)
+            {
+                break;
+            }
+            list.Add(topic);
+            dayIndex++;
+        }
+
+        await Verify(list);
+    }
+
+}
